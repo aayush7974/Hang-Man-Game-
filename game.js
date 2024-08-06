4015ec8726e50d03793995a946947730b4c8c0b0
Aayush Verma <147973691+aayush7974@users.noreply.github.com>
2024-08-06 13:12:08 +0530

    js file

---
+++ game.js
@@ -0,0 +1,262 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "HTML",
+        hint: "Hyper-Text-Markup-Language short form "
+    },
+    {
+        word: "six",
+        hint: "How many sizes of headers are available in HTML by default"
+    },
+    {
+        word: "ol",
+        hint: "How to create an ordered list in HTML"
+    },
+    {
+        word: ".html",
+        hint: "HTML files are saved by default with the extension"
+    },
+    {
+        word: "id",
+        hint: "Which attribute is used to provide a unique name to an HTML element."
+    },
+    {
+        word: "css",
+        hint: "Cascade style sheets short form"
+    },
+    {
+        word: "color",
+        hint: "The property in CSS used to change the text color of an element is -"
+    },
+    {
+        word: "style",
+        hint: "The HTML attribute used to define the inline styles is"
+    },
+    {
+        word: "padding",
+        hint: "Which of the following property is used as the shorthand property for the padding properties?"
+    },
+    {
+        word: "margin",
+        hint: "Which of the following property is used as the shorthand property of margin properties"
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for hardness."
+    },
+    {
+        word: "opacity",
+        hint: " The CSS property used to specify the transparency of an element is "
+    },
+    {
+        word: "display",
+        hint: "Which of the following CSS property is used for controlling the layout"
+    },
+    {
+        word: "parent",
+        hint: "What does the inherit value for a property do."
+    },
+    {
+        word: "Zindex",
+        hint: "Which of the following CSS properties specifies the stack order of elements"
+    },
+    {
+        word: "clip",
+        hint: "Which CSS property specifies the painting area of the background"
+    },
+    {
+        word: "var",
+        hint: "What function is used to insert values of a CSS variable"
+    },
+    {
+        word: "four",
+        hint: "How many layout modes were there before the Flexbox Layout Module was introduced in CSS."
+    },
+    {
+        word: "const",
+        hint: "How can a datatype be declared to be a constant type in javascript"
+    },
+    {
+        word: "in",
+        hint: "What keyword is used to check whether a given property is valid or not"
+    },
+    {
+        word: "object",
+        hint: " When an operator’s value is NULL, the typeof returned by the unary operator is:"
+    },
+    {
+        word: "stringify",
+        hint: "Which function is used to serialize an object into a JSON string in Javascript"
+    },
+    {
+        word: "async",
+        hint: "What keyword is used to declare an asynchronous function in Javascript"
+    },
+    {
+        word: "base",
+        hint: "Which object in Javascript doesn’t have a prototype"
+    },
+    {
+        word: "function",
+        hint: "Which one of the following is the correct way for calling the JavaScript code"
+    },
+    {
+        word: "mutable",
+        hint: "Which of the following type of a variable is volatile."
+    },
+    {
+        word: "invocation",
+        hint: "Which one of the following is used for the calling a function or a method in the JavaScript"
+    },
+    {
+        word: "true",
+        hint: "is Bootstrap3 mobile-first."
+    },
+    {
+        word: "rainbow",
+        hint: "A meteorological phenomenon that is caused by reflection, refraction, and dispersion of light."
+    },
+    {
+        word: "bind",
+        hint: "Which of the following jQuery method is used to attach a handler to an even."
+    },
+    {
+        word: "local",
+        hint: "if the names of variables are the same, then which of the following type of variable takes precedence"
+    },
+    {
+        word: "indentation",
+        hint: "Which of the following is used to define a block of code in Python language."
+    },
+    {
+        word: "define",
+        hint: "Which keyword is used for function in Python language."
+    },
+    {
+        word: "lambda",
+        hint: "Python supports the creation of anonymous functions at runtime, using a construct called."
+    },
+    {
+        word: "print",
+        hint: "Which of the following functions is a built-in function in python"
+    },
+    {
+        word: "function",
+        hint: "What is the method inside the class in python language"
+    },
+    {
+        word: "parentheses",
+        hint: "Which one of the following has the highest precedence in the expression."
+    },
+    {
+        word: "try",
+        hint: "Which of the following blocks allows you to test the code blocks for errors."
+    },
+    {
+        word: "dictionary",
+        hint: "As what datatype are the *kwargs stored, when passed into a function"
+    },
+    {
+        word: "datetime",
+        hint: "Which of the following modules need to be imported to handle date time computations in Python."
+    },
+    {
+        word: "true",
+        hint: "Float type of data type is represented by the float class."
+    },
+    {
+        word: "upper",
+        hint: "How to convert the lowercase letters in the string to uppercase in Python"
+    },
+    {
+        word: "dictionary",
+        hint: " represents key-value pair in Python"
+    },
+    {
+        word: "global",
+        hint: "the variable which is defined outside the function is called."
+    },
+    {
+        word: "creation",
+        hint: "Parameters of a function can be defined."
+    },
+    {
+        word: "ordered",
+        hint: "Elements of a list are"
+    },
+    {
+        word: "extend",
+        hint: "The method we used to attach over one element individually in a list is."
+    },
+    {
+        word: "add",
+        hint: "To attach an extra element in a set we use"
+    },
+    {
+        word: "remove",
+        hint: "Method that is used to delete an element from a set is"
+    },
+    {
+        word: "intersection",
+        hint: "Method that is used to delete an element from a set isA statement or situation that contradicts itself or defies intuition."
+    },
+    {
+        word: "colon",
+        hint: "In a dictionary keys and values are separated by."
+    },
+    {
+        word: "comma",
+        hint: "In a dictionary every key-value pairs are separated from each other by"
+    },
+    {
+        word: "shadow",
+        hint: "A dark area or shape produced by an object blocking the light."
+    },
+    {
+        word: "secret",
+        hint: "Something kept hidden or unknown to others."
+    },
+    {
+        word: "curiosity",
+        hint: "A strong desire to know or learn something."
+    },
+    {
+        word: "unpredictable",
+        hint: "Not able to be foreseen or known beforehand; uncertain."
+    },
+    {
+        word: "obfuscate",
+        hint: "To confuse or bewilder someone; to make something unclear or difficult to understand."
+    },
+    {
+        word: "unveil",
+        hint: "To make known or reveal something previously secret or unknown."
+    },
+    {
+        word: "illusion",
+        hint: "A false perception or belief; a deceptive appearance or impression."
+    },
+    {
+        word: "moonlight",
+        hint: "The light from the moon."
+    },
+    {
+        word: "vibrant",
+        hint: "Full of energy, brightness, and life."
+    },
+    {
+        word: "nostalgia",
+        hint: "A sentimental longing or wistful affection for the past."
+    },
+    {
+        word: "brilliant",
+        hint: "Exceptionally clever, talented, or impressive."
+    },
+];
